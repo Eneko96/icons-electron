483454f5f07ec3b9747824fd187090c2c6023c69
Eneko96 <imr.aem2526@gmail.com>
2023-09-23 19:31:46 +0100

    added infinite scroll

--- client/renderer.js
+++ client/renderer.js
@@ -1,74 +1,117 @@
 const $ = (selector) => document.querySelector(selector);
+let count = 60;
+let gFilenames = [];
 
-window.electronAPI.on("svg-filenames", (filenames) => {
+const addIcon = (file) => {
   const iconList = $(".icon-list");
 
-  // create a function for this and an Observer for loading more icons
-  // and then call it on the filenames event
+  const listItem = document.createElement("div");
+  listItem.className = "icon-list-item";
+  iconList.appendChild(listItem);
 
-  filenames.slice(0, 60).forEach((file) => {
-    console.log(file);
-    const listItem = document.createElement("div");
-    listItem.className = "icon-list-item";
-    iconList.appendChild(listItem);
+  const tempContainer = document.createElement("div");
+  tempContainer.innerHTML = file.content;
+  const svg = tempContainer.querySelector("svg");
+  svg.className = "svg-icon";
+  const text = document.createElement("span");
+  text.className = "icon-text";
+  text.textContent = file.name;
+  listItem.appendChild(svg);
+  listItem.appendChild(text);
 
-    const tempContainer = document.createElement("div");
-    tempContainer.innerHTML = file.content;
-    const svg = tempContainer.querySelector("svg");
-    svg.className = "svg-icon";
-    const text = document.createElement("span");
-    text.className = "icon-text";
-    text.textContent = file.name;
-    listItem.appendChild(svg);
-    listItem.appendChild(text);
+  listItem.addEventListener("click", () => {
+    const dialog = document.createElement("dialog");
+    const title = document.createElement("h5");
+    const content = document.createElement("pre");
+    const code = document.createElement("code");
+    const copyButton = document.createElement("button");
 
-    listItem.addEventListener("click", () => {
-      const dialog = document.createElement("dialog");
-      const title = document.createElement("h5");
-      const content = document.createElement("pre");
-      const code = document.createElement("code");
-      const copyButton = document.createElement("button");
+    copyButton.textContent = "Copy";
+    copyButton.className = "copy-button";
 
-      copyButton.textContent = "Copy";
-      copyButton.className = "copy-button";
+    const tooltip = document.createElement("span");
+    tooltip.textContent = "Copied!";
+    tooltip.className = "tooltip";
 
-      const tooltip = document.createElement("span");
-      tooltip.textContent = "Copied!";
-      tooltip.className = "tooltip";
+    copyButton.addEventListener("click", () => {
+      navigator.clipboard.writeText(file.content);
+      copyButton.appendChild(tooltip);
+      tooltip.classList.add("show");
+      const tooltipPosition = copyButton.getBoundingClientRect();
+      if (document.body.clientWidth - tooltipPosition.left < 100) {
+        tooltip.style.right = "0";
+      }
+      setTimeout(() => {
+        copyButton.removeChild(tooltip);
+      }, 1000);
+    });
 
-      copyButton.addEventListener("click", () => {
-        navigator.clipboard.writeText(file.content);
-        copyButton.appendChild(tooltip);
-        tooltip.classList.add("show");
-        const tooltipPosition = copyButton.getBoundingClientRect();
-        if (document.body.clientWidth - tooltipPosition.left < 100) {
-          tooltip.style.right = "0";
-        }
-        setTimeout(() => {
-          copyButton.removeChild(tooltip);
-        }, 1000);
-      });
+    dialog.appendChild(copyButton);
+    title.textContent = file.name;
+    title.className = "dialog-title";
 
-      dialog.appendChild(copyButton);
-      title.textContent = file.name;
-      title.className = "dialog-title";
+    dialog.appendChild(title);
+    dialog.className = "dialog";
 
-      dialog.appendChild(title);
-      dialog.className = "dialog";
+    content.className = "dialog-content";
+    code.textContent = file.content;
+    content.appendChild(code);
+    dialog.appendChild(content);
 
-      content.className = "dialog-content";
-      code.textContent = file.content;
-      content.appendChild(code);
-      dialog.appendChild(content);
+    document.body.appendChild(dialog);
+    dialog.showModal();
 
-      document.body.appendChild(dialog);
-      dialog.showModal();
-
-      dialog.addEventListener("click", (event) => {
-        if (event.target === dialog) {
-          dialog.close();
-        }
-      });
+    dialog.addEventListener("click", (event) => {
+      if (event.target === dialog) {
+        dialog.close();
+      }
     });
   });
+};
+
+window.electronAPI.on("svg-filenames", (filenames) => {
+  gFilenames = filenames;
+  filenames.slice(0, 60).forEach((file) => {
+    addIcon(file);
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const iconList = document.querySelector(".icon-list");
+
+  if (!iconList) {
+    console.error("Icon list not found");
+    return;
+  }
+
+  const handleIntersection = (lastElement) => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      for (let entry of entries) {
+        if (entry.isIntersecting) {
+          console.log("Last element is now visible!");
+          count = count + 60;
+          gFilenames.slice(count, count + 60).forEach((file) => addIcon(file));
+          intersectionObserver.disconnect();
+          handleIntersection(iconList.lastElementChild);
+          break;
+        }
+      }
+    });
+
+    intersectionObserver.observe(lastElement);
+  };
+
+  const mutationObserver = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      if (mutation.addedNodes.length) {
+        const lastElement = iconList.lastElementChild;
+        if (lastElement) {
+          handleIntersection(lastElement);
+          break;
+        }
+      }
+    }
+  });
+
+  mutationObserver.observe(iconList, { childList: true });
 });
