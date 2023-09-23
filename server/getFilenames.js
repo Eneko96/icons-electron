2aada03742430b198f7eef872865f57d65dfaad8
Eneko96 <imr.aem2526@gmail.com>
2023-09-24 00:02:48 +0100

    chore: remove console log

--- server/getFilenames.js
+++ server/getFilenames.js
@@ -9,7 +9,6 @@ const paths = {
 const dir = (type) => path.join(__dirname, paths[type]);
 
 exports.fileNames = (type) => {
-  console.log("on filenames");
   const fileNames = fs.readdirSync(dir(type));
   return fileNames.map((file) => {
     const data = fs.readFileSync(path.join(dir(type), file), "utf8");
