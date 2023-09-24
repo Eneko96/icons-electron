9d3359371327c354d75492637d83ab8b67b3df76
Eneko96 <imr.aem2526@gmail.com>
2023-09-25 00:09:18 +0100

    feat: remove - from text

--- client/renderer.js
+++ client/renderer.js
@@ -15,7 +15,7 @@ const addIcon = (file) => {
   svg.className = "svg-icon";
   const text = document.createElement("span");
   text.className = "icon-text";
-  text.textContent = file.name;
+  text.textContent = file.name.replaceAll("-", " ");
   listItem.appendChild(svg);
   listItem.appendChild(text);
 
@@ -93,7 +93,7 @@ const search = (input, iconList) => {
   }
   if (!ezFind) {
     gFilenames.forEach((file) => {
-      if (file.name.toLowerCase().indexOf(query) !== -1) {
+      if (file.name.toLowerCase().indexOf(query.replaceAll(" ", "-")) !== -1) {
         addIcon(file);
         ezFind = true;
       }
